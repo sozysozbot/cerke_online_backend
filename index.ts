b12f3fcacee6f850ca4b1bb53b1107b737046129
hsjoihs <hs.ioling.hs@gmail.com>
2019-07-31 23:58:20 +0900

    much nicer with io-ts

--- index.ts
+++ index.ts
@@ -15,6 +15,77 @@ import Ciurl = type__message.Ciurl;
 import Ret_InfAfterStep = type__message.Ret_InfAfterStep;
 import Ret_NormalMove = type__message.Ret_NormalMove;
 import Ret_AfterHalfAcceptance = type__message.Ret_AfterHalfAcceptance;
+import * as t from "io-ts";
+import { pipe } from 'fp-ts/lib/pipeable'
+import { fold } from 'fp-ts/lib/Either'
+
+const ColorVerifier = t.union([t.literal(0), t.literal(1)]);
+const ProfessionVerifier = t.union([
+  t.literal(0), t.literal(1), t.literal(2),
+  t.literal(3), t.literal(4), t.literal(5),
+  t.literal(6), t.literal(7), t.literal(8),
+  t.literal(9)]);
+
+const AbsoluteRowVerifier = t.union([
+  t.literal(0), t.literal(1), t.literal(2),
+  t.literal(3), t.literal(4), t.literal(5),
+  t.literal(6), t.literal(7), t.literal(8)]);
+
+const AbsoluteColumnVerifier = t.union([
+  t.literal(0), t.literal(1), t.literal(2),
+  t.literal(3), t.literal(4), t.literal(5),
+  t.literal(6), t.literal(7), t.literal(8)]);
+
+const AbsoluteCoordVerifier = t.tuple([AbsoluteRowVerifier, AbsoluteColumnVerifier]);
+
+const InfAfterStepVerifier = t.strict({
+  type: t.literal('InfAfterStep'),
+  color: ColorVerifier,
+  prof: ProfessionVerifier,
+  src: AbsoluteCoordVerifier,
+  step: AbsoluteCoordVerifier,
+  plannedDirection: AbsoluteCoordVerifier
+});
+
+const AfterHalfAcceptanceVerifier = t.strict({
+  type: t.literal('AfterHalfAcceptance'),
+  dest: t.union([AbsoluteCoordVerifier, t.null])
+});
+
+const NormalNonTamMoveVerifier = t.strict({
+  type: t.literal('NonTamMove'),
+  data: t.union([t.strict({
+    type: t.literal('FromHand'),
+    color: ColorVerifier,
+    prof: ProfessionVerifier,
+    dest: AbsoluteCoordVerifier
+  }), t.strict({
+    type: t.literal('SrcDst'),
+    src: AbsoluteCoordVerifier,
+    dest: AbsoluteCoordVerifier
+  }), t.strict({
+    type: t.literal('SrcStepDstFinite'),
+    src: AbsoluteCoordVerifier,
+    step: AbsoluteCoordVerifier,
+    dest: AbsoluteCoordVerifier
+  })])
+});
+
+const TamMoveVerifier = t.strict({
+  type: t.literal('TamMove'),
+  stepStyle: t.union([t.literal('NoStep'), t.literal('StepsDuringFormer'), t.literal('StepsDuringLatter')]),
+  src: AbsoluteCoordVerifier,
+  firstDest: AbsoluteCoordVerifier,
+  secondDest: AbsoluteCoordVerifier
+});
+
+
+const Verfier = t.union([
+  InfAfterStepVerifier,
+  AfterHalfAcceptanceVerifier,
+  NormalNonTamMoveVerifier,
+  TamMoveVerifier
+]);
 
 const PORT = process.env.PORT || 23564;
 const bodyParser = require('body-parser');
@@ -23,60 +94,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
-
-type TypeObj = { type: string };
-
-function isColor(arg: any): arg is Color {
-  return arg === 0 || arg === 1;
-}
-
-function isProfession(arg: any): arg is Profession {
-  return arg === 0
-    || arg === 1
-    || arg === 2
-    || arg === 3
-    || arg === 4
-    || arg === 5
-    || arg === 6
-    || arg === 7
-    || arg === 8
-    || arg === 9
-    ;
-}
-
-function isAbsoluteRow(arg: any): arg is AbsoluteRow {
-  return arg === 0
-    || arg === 1
-    || arg === 2
-    || arg === 3
-    || arg === 4
-    || arg === 5
-    || arg === 6
-    || arg === 7
-    || arg === 8
-    ;
-}
-
-function isAbsoluteColumn(arg: any): arg is AbsoluteColumn {
-  return arg === 0
-    || arg === 1
-    || arg === 2
-    || arg === 3
-    || arg === 4
-    || arg === 5
-    || arg === 6
-    || arg === 7
-    || arg === 8
-    ;
-}
-
-function isAbsoluteCoord(arg: any): arg is AbsoluteCoord {
-  return arg instanceof Array && arg.length === 2 && isAbsoluteRow(arg[0]) && isAbsoluteColumn(arg[1]);
-}
-
-function isTypeObj(arg: any): arg is TypeObj {
-  return arg != null && 'string' === typeof arg.type;
-}
 
 function analyzeAfterHalfAcceptance(msg: AfterHalfAcceptance): Ret_AfterHalfAcceptance {
   return ({
@@ -109,120 +126,41 @@ function analyzeInfAfterStep(msg: InfAfterStep): Ret_InfAfterStep {
   } as Ret_InfAfterStep);
 }
 
-function analyzeMessage(message: object): null | Ret_InfAfterStep | Ret_AfterHalfAcceptance | Ret_NormalMove {
-  if (!isTypeObj(message)) {
-    return {
-      legal: false,
-      whyIllegal: "Invalid message format: The message either does not have `.type` or its `.type` is not a string"
-    };
-  }
+function analyzeMessage(message: object): Ret_InfAfterStep | Ret_AfterHalfAcceptance | Ret_NormalMove {
+  const onLeft = (errors: t.Errors): Ret_InfAfterStep | Ret_AfterHalfAcceptance | Ret_NormalMove => ({
+    legal: false,
+    whyIllegal: `Invalid message format: ${errors.length} error(s) found during parsing`
+  })
 
-  if (message.type === 'InfAfterStep') { /* InfAfterStep */
-    const hasAdequateColor = function (arg: any): arg is ({ color: Color }) {
-      return arg != null && "color" in arg && isColor(arg.color);
-    }
-
-    if (!hasAdequateColor(message)) {
-      return {
-        legal: false,
-        whyIllegal: "Invalid message format: The message has `InfAfterStep` as its `.type` but does not have a valid `.color`"
-      };
-    }
-
-    const hasAdequateProf = function (arg: any): arg is ({ prof: Profession }) {
-      return arg != null && "prof" in arg && isProfession(arg.prof);
-    }
-
-    if (!hasAdequateProf(message)) {
-      return {
-        legal: false,
-        whyIllegal: "Invalid message format: The message has `InfAfterStep` as its `.type` but does not have a valid `.prof`"
-      };
-    }
-
-    const hasAdequateSrc = function (arg: any): arg is ({ src: AbsoluteCoord }) {
-      return arg != null && "src" in arg && isAbsoluteCoord(arg.src);
-    }
-
-    if (!hasAdequateSrc(message)) {
-      return {
-        legal: false,
-        whyIllegal: "Invalid message format: The message has `InfAfterStep` as its `.type` but does not have a valid `.src`"
+  return pipe(
+    Verfier.decode(message),
+    fold(onLeft, function (msg: InfAfterStep | AfterHalfAcceptance | NormalMove) {
+      if (msg.type === 'InfAfterStep') { /* InfAfterStep */
+        return analyzeInfAfterStep(msg);
+      } else if (msg.type === 'AfterHalfAcceptance') {
+        return analyzeAfterHalfAcceptance(msg);
+      } else if (msg.type === 'NonTamMove' || msg.type === 'TamMove') {
+        return ({
+          legal: true,
+          dat: Math.random() < 0.5 ? {
+            waterEntryHappened: true,
+            ciurl: [
+              Math.random() < 0.5,
+              Math.random() < 0.5,
+              Math.random() < 0.5,
+              Math.random() < 0.5,
+              Math.random() < 0.5
+            ] as Ciurl
+          } : {
+              waterEntryHappened: false
+            }
+        } as Ret_NormalMove);
+      } else {
+        let _should_not_reach_here: never = msg;
+        throw new Error("should not reach here");
       }
-    }
-
-    const hasAdequateStep = function (arg: any): arg is ({ step: AbsoluteCoord }) {
-      return arg != null && "step" in arg && isAbsoluteCoord(arg.step);
-    }
-
-    if (!hasAdequateStep(message)) {
-      return {
-        legal: false,
-        whyIllegal: "Invalid message format: The message has `InfAfterStep` as its `.type` but does not have a valid `.step`"
-      }
-    }
-
-    const hasAdequatePlannedDirection = function (arg: any): arg is ({ plannedDirection: AbsoluteCoord }) {
-      return arg != null && "plannedDirection" in arg && isAbsoluteCoord(arg.plannedDirection);
-    }
-
-    if (!hasAdequatePlannedDirection(message)) {
-      return {
-        legal: false,
-        whyIllegal: "Invalid message format: The message has `InfAfterStep` as its `.type` but does not have a valid `.step`"
-      }
-    }
-
-    return analyzeInfAfterStep({
-      type: 'InfAfterStep',
-      prof: message.prof,
-      color: message.color,
-      src: message.src,
-      step: message.step,
-      plannedDirection: message.plannedDirection
-    });
-  } else if (message.type === 'AfterHalfAcceptance') { /* AfterHalfAcceptance */
-
-    const hasAdequateDest = function (arg: any): arg is ({ dest: null | AbsoluteCoord }) {
-      return arg != null && "dest" in arg && (arg.dest === null || isAbsoluteCoord(arg.dest));
-    };
-
-    if (!hasAdequateDest(message)) {
-      return {
-        legal: false,
-        whyIllegal: "Invalid message format: The message has `AfterHalfAcceptance` as its `.type` but does not have a valid `.dest`"
-      };
-    }
-
-    const msg: AfterHalfAcceptance = {
-      dest: message.dest,
-      type: 'AfterHalfAcceptance'
-    }
-
-    return analyzeAfterHalfAcceptance(msg);
-
-  } else if (message.type === 'NonTamMove' || message.type === 'TamMove') { /* NormalMove */
-    return ({
-      legal: true,
-      dat: Math.random() < 0.5 ? {
-        waterEntryHappened: true,
-        ciurl: [
-          Math.random() < 0.5,
-          Math.random() < 0.5,
-          Math.random() < 0.5,
-          Math.random() < 0.5,
-          Math.random() < 0.5
-        ] as Ciurl
-      } : {
-          waterEntryHappened: false
-        }
-    } as Ret_NormalMove);
-  } else {
-    return {
-      legal: false,
-      whyIllegal: `Invalid message format: The message has an unrecognised \`.type\`, which is \`${message.type}\`.`
-    };
-  }
+    })
+  );
 }
 
 const app = express();
