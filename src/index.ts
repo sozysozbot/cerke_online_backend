8562c20d597308ca17529e18c2476efe7d9e586a
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-04-17 21:03:47 +0900

    only take the first 7

--- src/index.ts
+++ src/index.ts
@@ -248,7 +248,7 @@ const pool = new Pool({
   ssl: true,
 });
 
-const sha256 = (str: string) => crypto.createHash('sha256').update(str, 'utf8').digest('hex');
+const sha256_first7 = (str: string) => crypto.createHash('sha256').update(str, 'utf8').digest('hex').slice(0, 7);
 
 const { getPiece, setPiece } = (() => {
   function fromAbsoluteCoord_([absrow, abscol]: AbsoluteCoord): [
@@ -1136,7 +1136,7 @@ const random_entrance = (() => {
             // not yet assigned a room, but is in the waiting list
             waiting_list.delete(access_token);
             console.log(`Canceled ${access_token}.`);
-            publicly_announce(`Canceled ${sha256(access_token)}.`);
+            publicly_announce(`Canceled ${sha256_first7(access_token)}.`);
             return {
               legal: true,
               cancellable: true,
@@ -1395,7 +1395,7 @@ const random_entrance = (() => {
         `Opened a room ${room_id} to be used by ${newToken} and ${token}.`,
       );
       publicly_announce(
-        `Opened a room ${sha256(room_id)} to be used by ${sha256(newToken)} and ${sha256(token)}.`,
+        `Opened a room ${sha256_first7(room_id)} to be used by ${sha256_first7(newToken)} and ${sha256_first7(token)}.`,
       );
 
       console.log(
@@ -1405,7 +1405,7 @@ const random_entrance = (() => {
       );
       publicly_announce(
         `${
-          is_first_turn_newToken_turn[0 /* spring */] ? sha256(newToken) : sha256(token)
+          is_first_turn_newToken_turn[0 /* spring */] ? sha256_first7(newToken) : sha256_first7(token)
         } moves first.`,
       );
 
@@ -1416,7 +1416,7 @@ const random_entrance = (() => {
       );
       publicly_announce(
         `IA is down, from the perspective of ${
-          is_IA_down_for_newToken ? sha256(newToken) : sha256(token)
+          is_IA_down_for_newToken ? sha256_first7(newToken) : sha256_first7(token)
         }.`,
       );
 
@@ -1435,7 +1435,7 @@ const random_entrance = (() => {
       `Cannot find a partner for ${newToken}, who will thus be put in the waiting list.`,
     );
     publicly_announce(
-      `Cannot find a partner for ${sha256(newToken)}, who will thus be put in the waiting list.`,
+      `Cannot find a partner for ${sha256_first7(newToken)}, who will thus be put in the waiting list.`,
     );
     return {
       state: "in_waiting_list",
@@ -1768,7 +1768,7 @@ var room_to_gamestate = new Map<RoomId, GameState>();
 
 function open_a_room(token1: AccessToken, token2: AccessToken): RoomId {
   console.log("A match between", token1, "and", token2, "will begin.");
-  publicly_announce(`A match between ${sha256(token1)} and ${sha256(token2)} will begin.`)
+  publicly_announce(`A match between ${sha256_first7(token1)} and ${sha256_first7(token2)} will begin.`)
 
   // FIXME
   return uuidv4() as RoomId;
