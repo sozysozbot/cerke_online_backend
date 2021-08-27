cc8aacf7783e7251f7e74b109ca4e8278cfc3d44
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-08-28 01:17:57 +0900

    Hmmm

--- src/bot.ts
+++ src/bot.ts
@@ -85,29 +85,6 @@ function toPureGameState(
             if (p === "Tam2") {
                 return "Tam2"
             } else if (p && p.side === Side.IAOwner) {
-                return { color: p.color, prof: p.prof, side: cerke_verifier.Side.Downward }
-            } else if (p && p.side === Side.NonIAOwner) {
-                return { color: p.color, prof: p.prof, side: cerke_verifier.Side.Upward }
-            } else {
-                return null;
-            }
-        }));
-        const currentBoard: cerke_verifier.Board = cerke_verifier.rotateBoard(currentBoard_ as cerke_verifier.Board);
-        return {
-            IA_is_down: false,
-            tam_itself_is_tam_hue: game_state.tam_itself_is_tam_hue,
-            f: {
-                hop1zuo1OfUpward: game_state.f.hop1zuo1OfNonIAOwner.map(p => ({ color: p.color, prof: p.prof, side: cerke_verifier.Side.Upward })),
-                hop1zuo1OfDownward: game_state.f.hop1zuo1OfIAOwner.map(p => ({ color: p.color, prof: p.prof, side: cerke_verifier.Side.Downward })),
-                currentBoard
-            },
-            opponent_has_just_moved_tam
-        }
-    } else {
-        const currentBoard_: (cerke_verifier.Piece | null)[][] = game_state.f.currentBoard.map(row => row.map(p => {
-            if (p === "Tam2") {
-                return "Tam2"
-            } else if (p && p.side === Side.IAOwner) {
                 return { color: p.color, prof: p.prof, side: cerke_verifier.Side.Upward }
             } else if (p && p.side === Side.NonIAOwner) {
                 return { color: p.color, prof: p.prof, side: cerke_verifier.Side.Downward }
@@ -123,6 +100,29 @@ function toPureGameState(
                 // When IA_is_down, IA is owned by Upward
                 hop1zuo1OfUpward: game_state.f.hop1zuo1OfIAOwner.map(p => ({ color: p.color, prof: p.prof, side: cerke_verifier.Side.Upward })),
                 hop1zuo1OfDownward: game_state.f.hop1zuo1OfNonIAOwner.map(p => ({ color: p.color, prof: p.prof, side: cerke_verifier.Side.Downward })),
+                currentBoard
+            },
+            opponent_has_just_moved_tam
+        }
+    } else {
+        const currentBoard_: (cerke_verifier.Piece | null)[][] = game_state.f.currentBoard.map(row => row.map(p => {
+            if (p === "Tam2") {
+                return "Tam2"
+            } else if (p && p.side === Side.IAOwner) {
+                return { color: p.color, prof: p.prof, side: cerke_verifier.Side.Downward }
+            } else if (p && p.side === Side.NonIAOwner) {
+                return { color: p.color, prof: p.prof, side: cerke_verifier.Side.Upward }
+            } else {
+                return null;
+            }
+        }));
+        const currentBoard: cerke_verifier.Board = cerke_verifier.rotateBoard(currentBoard_ as cerke_verifier.Board);
+        return {
+            IA_is_down: false,
+            tam_itself_is_tam_hue: game_state.tam_itself_is_tam_hue,
+            f: {
+                hop1zuo1OfUpward: game_state.f.hop1zuo1OfNonIAOwner.map(p => ({ color: p.color, prof: p.prof, side: cerke_verifier.Side.Upward })),
+                hop1zuo1OfDownward: game_state.f.hop1zuo1OfIAOwner.map(p => ({ color: p.color, prof: p.prof, side: cerke_verifier.Side.Downward })),
                 currentBoard
             },
             opponent_has_just_moved_tam
@@ -149,16 +149,10 @@ export function generateBotMove(
     opponent_has_just_moved_tam: boolean,
     ia_is_down_for_player_not_bot: boolean
 ): BotMove {
-    if (ia_is_down_for_player_not_bot) {
-        console.log("The player should be controlling a black king, and the bot should be controlling a red king.")
-    } else {
-        console.log("The player should be controlling a red king, and the bot should be controlling a black king.")
-    }
-
     // the moves that are generated are the opponent's move, so I need a negation
-    const pure_game_state = toPureGameState(game_state, opponent_has_just_moved_tam, !ia_is_down_for_player_not_bot);
+    const pure_game_state = toPureGameState(game_state, opponent_has_just_moved_tam, ia_is_down_for_player_not_bot);
 
-    const candidates = not_from_hand_candidates(pure_game_state); 
+    const candidates = not_from_hand_candidates(pure_game_state);
     while (true) {
         const mov = candidates[candidates.length * Math.random() | 0];
         if (mov.type === "InfAfterStep" || mov.type === "TamMove") { continue; }
