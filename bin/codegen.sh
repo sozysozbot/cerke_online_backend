2a6d4c0c3874dadd15d7103589d0e58c10c9982d
kgtkr <contact@kgtkr.net>
2022-02-02 14:39:12 +0900

    fixup! codegen

--- bin/codegen.sh
+++ bin/codegen.sh
@@ -3,7 +3,7 @@
 rm -rf src/codegen
 mkdir -p src/codegen
 
-TMP=`mktemp --suffix=.yaml`
+TMP=`mktemp`
 # 対応: https://github.com/drwpow/openapi-typescript/issues/462
 sed 's/anyOf/oneOf/' openapi.yaml > $TMP
 npx openapi-typescript $TMP --output src/codegen/openapi.ts
